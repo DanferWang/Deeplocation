954ba4293de1e205085a694a54e436247781bb3d
DanferWang <danferwang1123@gmail.com>
2020-10-20 13:56:24 +0200

    test3000

--- test3000/train.py
+++ test3000/train.py
@@ -131,7 +131,7 @@ def main(cfg):
 
 if __name__ == '__main__':
     # user define
-    root_path = "./Deeplocation/"
+    root_path = "./Deeplocation/test3000/"
 
     config_file = root_path + "example.json"
     logging.basicConfig(format='%(asctime)s %(levelname)s: %(message)s', datefmt='%d-%m-%Y %H:%M:%S',

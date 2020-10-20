40ea451d5c4832d2a79e20d6a5ae8ddd72a89e88
DanferWang <danferwang1123@gmail.com>
2020-10-20 13:54:01 +0200

    test3000

---
+++ test3000/dataloader.py
@@ -0,0 +1,213 @@
+import logging
+from pathlib import Path
+from typing import List, Tuple
+
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+
+AUTOTUNE = tf.data.experimental.AUTOTUNE
+
+
+class Partitioning:
+    """ Partitioning class to map class labels (i.e. s2 cells) to class indexes.
+
+    """
+
+    def __init__(self, csv_file: Path, shortname=None, skiprows=None):
+        """Read partitioning CSV and save number of classes and name of the partitioning
+
+        Args:
+            csv_file: path to partitioning CSV
+            shortname: short name otherwise shortname is filename without extension
+            skiprows: skip the first n rows of the CSV
+        """
+
+        logging.info(f'Loading partitioning from file: {csv_file}')
+        self._df = pd.read_csv(csv_file, index_col='hex_id', skiprows=skiprows)
+        self.nclasses = len(self._df.index)
+        self.name = csv_file.stem  # filename without extension
+        if shortname:
+            self.shortname = shortname
+        else:
+            self.shortname = self.name
+
+        self._hexids = self._df.index.tolist()
+
+    def __repr__(self):
+        return f'{self.name} short: {self.shortname} n: {self.nclasses}'
+
+    def get_class_idx(self, class_label) -> int:
+        """Map the class label i.e. hexid to an int index.
+
+        Args:
+            class_label: class label
+
+        Returns: class index
+
+        """
+        return self._df.loc[class_label, 'class_label']
+
+    def get_lat_lng(self, class_label: str) -> Tuple[float, float]:
+        """Return latitude and longitude given the class label
+
+        :param idx: class label
+        :return: mean latitude and longitude for given cell
+        """
+        row = self._df.loc[class_label]
+        lat = float(row['latitude_mean'])
+        lng = float(row['longitude_mean'])
+        return lat, lng
+
+    def get_hexid(self, idx):
+        return self._hexids[idx]
+
+
+def get_partitionings(base_folder: str, filenames: List[str], shortnames: List[str], skiprows=None) -> List[Partitioning]:
+    return [Partitioning(Path(base_folder) / fname, shortname, skiprows=skiprows) for fname, shortname in zip(filenames, shortnames)]
+
+
+class ImageDataset:
+
+    def __init__(self,
+                 dataset_path: str,
+                 image_base_dir: str,
+                 partitionings: List[Partitioning],
+                 nrows=None,
+                 batch_size=32,
+                 target_image_size=224,
+                 shuffle=False,
+                 validation=False,
+                 ):
+        """Wrapper class for a tf.data.Dataset
+
+        Args:
+            dataset_path: path to a CSV including img_path and one column with class indexes for each partitioning
+            image_base_dir: base folder where images are stored
+            partitionings: list of partitionings
+            nrows: consider only the first n rows from the dataset CSV (dataset_path)
+            batch_size: batch size
+            target_image_size: network input size
+            shuffle: whether the dataset will be shuffled (per instance)
+            validation: use a resized center crop as image pre-processing step instead of random crop
+        """
+
+        self.batch_size = batch_size
+        self.shuffle = shuffle
+        self.target_image_size = target_image_size
+        self._image_base_dir = Path(image_base_dir)
+        self._validation = validation
+
+        if not self._image_base_dir.exists():
+            raise FileNotFoundError(image_base_dir + ' is not a dataset directory')
+
+        logging.info(f'Read CSV from file: {dataset_path}')
+        self.df = pd.read_csv(dataset_path, nrows=nrows)
+        self.df['img_path'] = self.df['img_path'].apply(lambda p: str(self._image_base_dir / p))
+        self.nbatches = int(np.ceil(len(self) / batch_size))
+
+        logging.info('Prepare dataset...')
+        with tf.device('/cpu:0'):
+            ds_images = tf.data.Dataset.from_tensor_slices(self.df['img_path'].values)
+            #ds_p = [tf.data.Dataset.from_tensor_slices(self.df[p].values) for p in self.df.columns[1:].to_list()]
+            ds_p = [tf.data.Dataset.from_tensor_slices(self.df[p.name].values) for p in partitionings]
+
+            ds = tf.data.Dataset.zip((ds_images, *ds_p))
+            ds = ds.map(self._process_item, num_parallel_calls=AUTOTUNE)
+            self.ds = self._prepare_for_iterate(ds)
+
+    def __len__(self):
+        return len(self.df.index)
+
+    def _image_preprocessing(self, img_path):
+        """Load an JPEG image from file and process it
+
+        Distinguish between training and validation mode.
+
+        Args:
+            img_path: full image path to a JPEG encoded image
+
+        Returns: preprocessed image as Tensor
+
+        """
+        tf.device('/cpu:0')
+        img_encoded = tf.io.read_file(img_path)
+        img = tf.image.decode_jpeg(img_encoded, channels=3)  # decode_image does not return a tensor with shape
+        img = tf.image.convert_image_dtype(img, tf.float32)
+        img = tf.multiply(tf.subtract(img, 0.5), 2)
+
+        if self._validation:
+            # center crop after
+            # resizing the image preserving the aspect ratio
+            height = tf.cast(tf.shape(img)[0], dtype=tf.float32)
+            width = tf.cast(tf.shape(img)[1], dtype=tf.float32)
+            max_side_len = tf.maximum(width, height)
+            min_side_len = tf.minimum(width, height)
+            is_w, is_h = tf.cond(tf.less(width, height), lambda: (0, 1), lambda: (1, 0))
+            ratio = self.target_image_size / min_side_len
+            offset = (tf.cast(max_side_len * ratio + 0.5, dtype=tf.int32) - self.target_image_size) // 2
+            img = tf.image.resize(img,
+                                size=[tf.cast(height * ratio + 0.5, dtype=tf.int32),
+                                     tf.cast(width * ratio + 0.5, dtype=tf.int32)],
+                                     antialias=True)
+
+            bbox = [
+                is_h * offset, is_w * offset,
+                tf.constant(self.target_image_size),
+                tf.constant(self.target_image_size)
+            ]
+            img_crop = tf.image.crop_to_bounding_box(img, bbox[0], bbox[1], bbox[2], bbox[3])
+
+            return img_crop
+        else:
+            # random distorted crop
+            bbox = tf.image.sample_distorted_bounding_box(
+                tf.shape(img),
+                bounding_boxes=tf.constant(0, shape=[1, 0, 4]),
+                min_object_covered=0.5,
+                aspect_ratio_range=[0.75, 1.33],
+                area_range=[0.7, 1.0],
+                max_attempts=100,
+                use_image_if_no_bounding_boxes=True)
+            bbox_begin, bbox_size, distort_bbox = bbox
+
+            img = tf.slice(img, bbox_begin, bbox_size,)
+            img = tf.image.random_flip_left_right(img)
+
+        # resize crop to network size
+        img = tf.image.resize(img, [self.target_image_size, self.target_image_size])
+
+        return img
+
+    def _process_item(self, img_path, *class_indexes):
+        """Wrapper function that delivers a preprocessed image and target values
+
+        Args:
+            img_path: full path to an image file
+            *class_indexes: list of target values (one class index per partitionign)
+
+        Returns: (X, y)
+
+        """
+        tf.device('/cpu:0')
+        img = self._image_preprocessing(img_path)
+        return img, class_indexes
+
+    def _prepare_for_iterate(self, ds, cache=True, shuffle_buffer_size=1000):
+        # This is a small dataset, only load it once, and keep it in memory.
+        # use `.cache(filename)` to cache preprocessing work for datasets that don't
+        # fit in memory.
+        if cache:
+            if isinstance(cache, str):
+                ds = ds.cache(cache)
+        else:
+            ds = ds.cache()
+
+        ds = ds.repeat()
+        if self.shuffle:
+            ds = ds.shuffle(buffer_size=shuffle_buffer_size)
+        ds = ds.batch(self.batch_size)
+
+        # `prefetch` lets the dataset fetch batches in the background
+        ds = ds.prefetch(buffer_size=AUTOTUNE)
+        return ds
---
+++ test3000/train.py
@@ -0,0 +1,142 @@
+import json
+import logging
+from pathlib import Path
+from datetime import datetime
+from functools import partial
+from typing import List
+from shutil import copy
+
+import tensorflow as tf
+from tensorflow.keras.applications.resnet_v2 import ResNet101V2
+
+from dataloader import ImageDataset, Partitioning, get_partitionings
+
+
+def build_multi_partitioning_model(partitionings: List[Partitioning], checkpoint: None) -> tf.keras.models.Model:
+    """Build ResNet model with multiple classifier on top - one for each partitioning
+
+    :param partitionings: list of partitionings
+    :param checkpoint: path to model checkpoint
+
+    """
+    if not checkpoint:
+        # train from scratch using imagenet weights
+        base_model = ResNet101V2(input_shape=(224, 224, 3), weights='imagenet', include_top=False, pooling='avg')
+
+        classifiers = []  # one classifier for each partitioning on top
+        for p in partitionings:
+            c = tf.keras.layers.Dense(p.nclasses, activation=None, name=p.shortname + '_logits')(base_model.output)
+            c = tf.keras.activations.softmax(c)
+            classifiers.append(c)
+
+        # during optimization the losses for each classifier are added by default
+        return tf.keras.models.Model(inputs=base_model.input, outputs=classifiers)
+
+    else:
+        base_model = tf.keras.models.load_model(checkpoint)
+        base_model.summary()
+        if all([base_model.get_layer(p.shortname + '_logits').get_weights()[1].shape[0] == p.nclasses for p in
+                partitionings]):
+            return base_model
+        else:
+            raise ValueError('Shape mismatch for given classification layers and size of partitioning')
+
+
+def step_lr(epoch, base_lr, step_size, gamma=0.5):
+    """Decay the learning rate every step_size epochs
+
+    :param epoch: current epoch
+    :param base_lr: learning rate on training start
+    :param step_size: period of learning rate decay
+    :param gamma: multiplicative factor of learning rate decay, default: 0.5
+    :return: learning rate for current epoch
+    """
+
+    return base_lr * gamma ** (epoch // step_size)
+
+
+def main(cfg):
+    timestamp = datetime.now().strftime('%Y-%m-%d:%H-%M')
+    result_dir = Path(cfg['base_result_dir'], timestamp)
+    result_dir.mkdir(parents=True)
+
+    # save partitionings to result folder
+    for p_file in cfg['data']['partitioning']['filenames']:
+        copy(Path(cfg['data']['partitioning']['base_folder'], p_file).resolve(), result_dir.resolve() / p_file)
+    # copy config file
+    with open(result_dir / 'cfg.json', 'w') as fw:
+        json.dump(cfg, fw, indent=4)
+
+    logging.info('Initialize the partitionings...')
+    partitionings = get_partitionings(**cfg['data']['partitioning'])
+    logging.info(partitionings)
+
+    logging.info('Build the model...')
+    tf.device('/gpu:0')
+    # build model with n classifiers on top
+    # the total loss that will be minimized by the model will be the sum of all individual losses
+    model = build_multi_partitioning_model(
+        partitionings=partitionings,
+        checkpoint=cfg['training']['continue_from_checkpoint']
+    )
+    callbacks = []
+
+    # learning rate scheduler
+    if cfg['training']['lr_scheduler_activate']:
+        schedule_func = partial(step_lr, base_lr=cfg['training']['lr'], **cfg['training']['lr_scheduler'])
+        lr_scheduler = tf.keras.callbacks.LearningRateScheduler(schedule_func)
+        callbacks.append(lr_scheduler)
+
+    # optimizer
+    optim = tf.keras.optimizers.SGD(learning_rate=cfg['training']['lr'], momentum=0.9, decay=1e-4, nesterov=True)
+    model.compile(optimizer=optim, loss=tf.keras.losses.sparse_categorical_crossentropy)
+
+    logging.info('Read the training set...')
+    dataset_train = ImageDataset(**cfg['data']['train'], partitionings=partitionings)
+    logging.info(f'Number of images for training: {len(dataset_train)}')
+    logging.info(f'Number of batches for training: {dataset_train.nbatches}')
+
+    logging.info('Read the validation set...')
+    dataset_valid = ImageDataset(**cfg['data']['valid'], validation=True, partitionings=partitionings)
+
+    # tensorboard logging
+    log_dir = result_dir / 'logs'
+    log_dir.mkdir(parents=True, exist_ok=True)
+    tbc = tf.keras.callbacks.TensorBoard(log_dir=str(log_dir), update_freq=100, write_graph=False, profile_batch=0)
+    callbacks.append(tbc)
+
+    # save best model callback
+    if cfg['training']['save_checkpoints']:
+        tf.device('/cpu:0')
+        cc = tf.keras.callbacks.ModelCheckpoint(
+            str(result_dir / 'model-{epoch:02d}-{val_loss:.2f}.h5'),
+            monitor='val_loss',
+            save_best_only=False,
+            save_weights_only=False,
+            save_freq='epoch'
+        )
+        callbacks.append(cc)
+
+    # train the model
+    model.fit(
+        dataset_train.ds,
+        callbacks=callbacks,
+        steps_per_epoch=dataset_train.nbatches,
+        validation_data=dataset_valid.ds,
+        **cfg['training']['fit_params']
+    )
+
+
+    return
+
+if __name__ == '__main__':
+    # user define
+    root_path = "./Deeplocation/"
+
+    config_file = root_path + "example.json"
+    logging.basicConfig(format='%(asctime)s %(levelname)s: %(message)s', datefmt='%d-%m-%Y %H:%M:%S',
+                        level=logging.INFO)
+
+    cfg = json.load(open(config_file, 'r'))
+
+    main(cfg)
